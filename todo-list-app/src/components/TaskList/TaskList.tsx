a3adc5626b86d5389e9a57f6fb2343d4fe7733e3
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-03-15 01:33:47 +0700

    Add List

---
+++ todo-list-app/src/components/TaskList/TaskList.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Todo } from '../../@types/todo.type'
+import styles from './taskList.module.scss'
+
+interface TaskListProps {
+  doneTaskList: boolean
+  todos: Todo[]
+  handleDoneTodo: (id: string, done: boolean) => void
+}
+
+export default function TaskList(props: TaskListProps) {
+  const { doneTaskList, todos, handleDoneTodo } = props
+
+  // // use currying
+  // const onChangeCheckbox = (idTodo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   handleDoneTodo(idTodo, event.target.checked)
+  // }
+  // //below
+  // onChange={onChangeCheckbox(todo.id)}
+
+  return (
+    <div className='mb-2'>
+      <h2 className={styles.title}>{doneTaskList ? 'Completed' : 'Incompleted'}</h2>
+      <div className={styles.tasks}>
+        {todos.map((todo) => (
+          <div className={styles.task} key={todo.id}>
+            <input
+              type='checkbox'
+              className={styles.taskCheckbox}
+              checked={todo.done}
+              onChange={(event) => handleDoneTodo(todo.id, event.target.checked)}
+            />
+            <span className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ''}`}>{todo.name}</span>
+            <div className={styles.taskActions}>
+              <button className={styles.taskBtn}>🖊️</button>
+              <button className={styles.taskBtn}>🗑️</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ todo-list-app/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { Todo } from '../../@types/todo.type'
+import TaskInput from '../TaskInput/TaskInput'
+import TaskList from '../TaskList/TaskList'
+import styles from './todoList.module.scss'
+
+export default function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const doneTodos = todos.filter((todo) => todo.done)
+  const notDoneTodos = todos.filter((todo) => !todo.done)
+
+  const addTodo = (name: string) => {
+    const todo: Todo = {
+      name,
+      done: false,
+      id: new Date().toISOString()
+    }
+    setTodos((prev) => [...prev, todo])
+  }
+
+  const handleDoneTodo = (id: string, done: boolean) => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done }
+        }
+        return todo
+      })
+    })
+  }
+  console.log('todos', todos)
+  return (
+    <div className={styles.todoList}>
+      <div className={styles.todoListContainer}>
+        <TaskInput addTodo={addTodo} />
+        <TaskList doneTaskList={false} todos={notDoneTodos} handleDoneTodo={handleDoneTodo} />
+        <TaskList doneTaskList todos={doneTodos} handleDoneTodo={handleDoneTodo} />
+      </div>
+    </div>
+  )
+}
